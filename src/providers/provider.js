17c5c24fcfb3d6b86d24f1e637a183282998c9ce
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2020-09-10 12:35:36 -0300

    Add a torrent provider interface

---
+++ src/providers/provider.js
@@ -0,0 +1,106 @@
+import { YTSProvider } from './yts/provider';
+
+const providers = {
+    'yts': YTSProvider,
+};
+
+class TorrentMagnet {
+    /**
+     * @param {String} title The name of the magnet
+     * @param {String} link The actual link of the magnet
+     * @param {Number} size Torrent size in bytes
+     */
+    constructor(title, link, size = 0) {
+        this.title = title;
+        this.link = link;
+        this.size = size;
+    }
+}
+
+/**
+ * Information on some type of media, such as type (tv show, movie...),
+ * name, episode, season...
+ * ALL of the fiels may be null, so one must check before using anything
+ */
+class MediaInfo {
+    /**
+     * @param {Object} info Information about the media
+     * @param {String=} info.title The media title
+     * @param {'show'|'movie'=} info.type Whether the media represents
+     * @param {Number=} info.season The media season number. Ignore if type === 'movie'
+     * @param {Number=} info.episode The media episode number. Ignore if type === 'movie'
+     * @param {String=} info.imdb The media imdb id
+     * @param {Boolean=} info.isEntireSeason Whether this instance represents a entire season or not. Ignore if type ==='movie'
+     */
+    constructor(info) {
+        this.title = info.title;
+        this.type = info.type;
+        this.season = info.season;
+        this.episode = info.episode;
+        this.imdb = info.imdb;
+        this.isEntireSeason = info.isEntireSeason;
+    }
+}
+
+class PagedResponse {
+    /**
+     * @param {Object} response The response
+     * @param {Function<Promise<PagedResponse>>} next Callback that returns the next page. Null if there is no next page.
+     */
+    constructor(response, next) {
+        this.response = response;
+        this.next = next;
+    }
+}
+
+/**
+ * Abstract class for torrent providers
+ * Exists mostly for documentation purposes
+ */
+class Provider {
+
+    /**
+     * A mandatory parameterless constructor is mandaroty
+     * This should put the provider in a 'default' state
+     */
+    constructor() {
+
+    }
+
+    /**
+     * Here, the provider can load some previously saved configuration
+     * (stored, for exaple, in the browser's localStorage)
+     *
+     * This is garanteed to be called at least once before any other
+     * methods
+     */
+    loadConfig() {
+
+    }
+
+    /**
+     * Should return whether the provider is currently in a valid state
+     * (i.e it has all the data it needs in order to function)
+     * @returns {Boolean}
+     */
+    isValid() {
+
+    }
+
+    /**
+     * Searches for torrents for media 'info'
+     * @param {MediaInfo} info Info to use when querying services
+     * @param {Number=} page The page to look up using pagination. Should default to 1.
+     * @returns {PagedResponse<[]String>} PagedResponce of array of magnet links, in no particular order
+     */
+    async getMagnets(info, page) {
+
+    }
+}
+
+export {
+    Provider,
+    MediaInfo,
+    PagedResponse,
+    TorrentMagnet,
+}
---
+++ src/providers/yts/provider.js
@@ -0,0 +1,77 @@
+import YTS from './yts.js/yts';
+import { Provider, MediaInfo, PagedResponse, TorrentMagnet } from '../provider';
+
+export class YTSProvider extends Provider {
+    constructor() {
+        super();
+
+        this.yts = new YTS();
+    }
+
+    loadConfig() {
+        // We don't have any configuration to do right now
+    }
+
+    isValid() {
+        return true; // You're always valid! :trans_rights:
+    }
+
+    /**
+     * Searches for torrents for media 'info'
+     * @param {MediaInfo} info Info to use when querying services
+     * @param {Number=} page The page to look up using pagination. Should default to 1.
+     * @returns {PagedResponse<[]String>} PagedResponce of array of magnet links, in no particular order
+     */
+    async getMagnets(info, page = 1) {
+        console.log(info, page);
+
+        var query = '';
+
+        if (info.imdb) {
+            query = info.imdb;
+        } else if (info.title) {
+            query = info.title;
+        }
+
+        if (info.type == 'show' || query == '') {
+            // We don't have shows at yts
+            return new PagedResponse([], null);
+        }
+
+        const itemsPerPage = 20;
+
+        var data = await this.yts.searchMovies(query, page, itemsPerPage);
+        data.movies = data.movies || [];
+
+        var magnets = [];
+
+        const trackers = [
+            'udp://open.demonii.com:1337/announce',
+            'udp://tracker.openbittorrent.com:80',
+            'udp://tracker.coppersurfer.tk:6969',
+            'udp://glotorrents.pw:6969/announce',
+            'udp://tracker.opentrackr.org:1337/announce',
+            'udp://torrent.gresille.org:80/announce',
+            'udp://p4p.arenabg.com:1337',
+            'udp://tracker.leechers-paradise.org:6969',
+        ];
+
+        for (var movie of data.movies) {
+            const name = encodeURIComponent(movie.title_long);
+            for (var torrent of movie.torrents) {
+                var link = `magnet:?xt=urn:btih:${torrent.hash}&dn=${name}&tr=${trackers.join('&tr=')}`;
+                var title = `${movie.title_long}.${torrent.quality}.${torrent.type}`;
+                magnets.push(new TorrentMagnet(
+                    title,
+                    link,
+                    torrent.size_bytes,
+                ));
+            }
+        }
+
+        return new PagedResponse(
+            magnets,
+            data.movies.length < itemsPerPage ? null : () => this.getMagnets(info, page + 1),
+        );
+    }
+}
