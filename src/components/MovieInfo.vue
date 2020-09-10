8c059623fb2ab9698e70bae328447bc750f43268
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2020-09-10 14:54:58 -0300

    Display all download options

--- src/components/MovieInfo.vue
+++ src/components/MovieInfo.vue
@@ -1,43 +1,80 @@
 <template>
-    <div class="row">
-        <div class="col-12 col-md-4 pb-4">
-            <img v-if="poster" class="img-fluid rounded lifted" :src="poster" />
-        </div>
-        <div class="col-12 col-md-8">
-            <h1>
-                {{ movieDetails.title }}
-                <span v-if="year" class="text-muted">({{ year }})</span>
-            </h1>
-            <p :v-if="movieDetails.tagline != ''" class="text-muted">{{movieDetails.tagline}}</p>
-            <p class="text-justify">{{ movieDetails.overview }}</p>
-            <div class="row">
-                <div class="col-12 col-lg-6 mt-3">
-                    <YoutubeLink v-if="trailer" class="mx-1" :id="trailer" text="Trailer" />
-                    <TMDBLink class="mx-1" type="movie" :id="movieDetails.id" />
-                    <IMDBLink v-if="movieDetails.imdb_id" class="mx-1" :id="movieDetails.imdb_id" />
+    <div>
+        <div class="row">
+            <div class="col-12 col-md-4 pb-4">
+                <img v-if="poster" class="img-fluid rounded lifted" :src="poster" />
+            </div>
+            <div class="col-12 col-md-8">
+                <h1>
+                    {{ movieDetails.title }}
+                    <span v-if="year" class="text-muted">({{ year }})</span>
+                </h1>
+                <p :v-if="movieDetails.tagline != ''" class="text-muted">{{movieDetails.tagline}}</p>
+                <p class="text-justify">{{ movieDetails.overview }}</p>
+                <div class="row">
+                    <div class="col-12 col-lg-6 mt-3">
+                        <YoutubeLink v-if="trailer" class="mx-1" :id="trailer" text="Trailer" />
+                        <TMDBLink class="mx-1" type="movie" :id="movieDetails.id" />
+                        <IMDBLink
+                            v-if="movieDetails.imdb_id"
+                            class="mx-1"
+                            :id="movieDetails.imdb_id"
+                        />
+                    </div>
+                    <div class="col-12 col-lg-6 mt-3 text-center">
+                        <div v-if="torrents == null" class="font-weight-bold">
+                            Searching torrents...
+                            <LoadingSpinner class="mt-1" />
+                        </div>
+                        <div
+                            v-else-if="torrents.length <= 0"
+                            class="font-weight-bold"
+                        >We couldn't find any torrents.</div>
+                        <a
+                            v-else-if="bestMagnet != null"
+                            v-on:click="$root.downloadMagnet(bestMagnet)"
+                            class="btn btn-block btn-success font-weight-bold"
+                        >
+                            {{ bestMagnet.size | size }}
+                            <i class="zmdi zmdi-download"></i>
+                        </a>
+                    </div>
                 </div>
-                <div class="col-12 col-lg-6 mt-3 text-center">
-                    <div v-if="torrents == null" class="font-weight-bold">
-                        Searching torrents...
-                        <LoadingSpinner class="mt-1" />
-                    </div>
-                    <div v-else-if="torrents.length <= 0" class="font-weight-bold">
-                        We couldn't find any torrents.
-                    </div>
-                    <a
-                        v-else-if="bestMagnet != null"
-                        v-on:click="$root.downloadMagnet(bestMagnet)"
-                        class="btn btn-block btn-success font-weight-bold"
-                    >
-                        {{ bestMagnet.size | size }}
-                        <i class="zmdi zmdi-download"></i>
-                    </a>
+                <hr />
+                <div>
+                    <h5>You might also like:</h5>
+                    <MediaQuery
+                        v-if="related != null"
+                        v-bind:query="related"
+                        :component="MovieThumb"
+                    />
                 </div>
             </div>
-            <hr />
-            <div>
-                <h5>You might also like:</h5>
-                <MediaQuery v-if="related != null" v-bind:query="related" :component="MovieThumb" />
+        </div>
+        <div class="row" v-if="torrents != null && torrents.length > 0">
+            <div class="col-12">
+                <hr />
+                <h4>Download Options:</h4>
+
+                <div class="col-12 pr-0">
+                    <div
+                        v-for="torrent of torrents"
+                        :key="torrent.link"
+                        class="row col-12 py-3 striped"
+                    >
+                        <div class="">
+                            <a
+                                role="button"
+                                class="btn btn-success badge mr-1"
+                                v-on:click="$root.downloadMagnet(torrent)"
+                            >
+                                <i class="zmdi zmdi-download"></i>
+                            </a>
+                            <span class="badge py-1 badge-secondary torrent-info">{{torrent.size | size}}</span>
+                        </div>
+                        <div class="text-truncate col">{{torrent.title}}</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -140,7 +177,7 @@ export default {
     filters: {
         size(size) {
             var i = Math.floor(Math.log(size) / Math.log(1024));
-            return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+            return (size / Math.pow(1024, i)).toFixed(1) + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
         },
     },
     mounted() {
@@ -161,3 +198,12 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.striped:nth-child(even) {
+    background-color: #f8f9fa;
+}
+.torrent-info {
+    width: 6em;
+}
+</style>
